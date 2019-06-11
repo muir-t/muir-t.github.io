dde7a30e4ab59a3f6200b5d3a37f0776b9c031cf
Tom Muir <tom@tom-muir.co.uk>
2019-06-11 15:55:39 +0100

    Maybe works on Chrome and Firefox: Still broken on mobile and Safari

--- swave.js
+++ swave.js
@@ -7,36 +7,40 @@ var colors = {
 
 var return_acc = 0.5;
 
-function Point () {
+var analyser;
+
+function Point (colour) {
     this.x = 0;
     this.y = 0;
-    this.xv = (Math.floor((Math.random() * 50) + 1)-25)/10;
-    this.yv = (Math.floor((Math.random() * 50) + 1)-25)/10;
+    this.xv = (Math.floor((Math.random() * 50) + 1)-25)/20;
+    this.yv = (Math.floor((Math.random() * 50) + 1)-25)/20;
 
     this.alpha = 0.1;
+
+    // this.color = colour;
 }
 
 Point.prototype = {
   constructor: Point,
 
-  update_position: function(){
-    this.x += this.xv;
-    this.y += this.yv;
+  update_position: function(i){
+    this.x += this.xv*(-i/50);
+    this.y += this.yv*(-i/50);
   },
 
   update_velocity: function(){
     if (this.x <= -canvasW/2){
-      this.xv += return_acc;
+      this.xv *= -1;
     }
     if (this.x >= canvasW/2){
-      this.xv += -return_acc;
+      this.xv *= -1;
     }
 
     if (this.y <= -canvasH/2){
-      this.yv += return_acc;
+      this.yv *= -1;
     }
     if (this.y >= canvasH/2){
-      this.yv += -return_acc;
+      this.yv *= -1;
     }
   },
 
@@ -56,14 +60,31 @@ var nopoints = 100;
 var globalalpha = 0.9;
 
 function init() {
+
   var audio = document.getElementById("audio");
+  audio.crossOrigin = "anonymous";
   audio.autoplay = true;
-  audio.src = 'https://github.com/muir-t/muir-t.github.io/blob/master/Lorde_-_Tennis_Court_Flume_Remix.mp3?raw=true';
+  audio.src = 'Black_Coast_-_TRNDSTTR_(Lucian_Remix).mp3';
   audio.load();
+
   audio.oncanplaythrough = function() {
     if (audio.currentTime == 0) {
-      audio.currentTime = 90;
-      for (i = 0; i < nopoints; i++) {
+      audio.currentTime = 30;
+
+      var AudioContext = window.AudioContext || window.webkitAudioContext;
+      var context = new AudioContext();
+      var src = context.createMediaElementSource(audio);
+      analyser = context.createAnalyser();
+
+      src.connect(analyser);
+      analyser.connect(context.destination);
+
+      analyser.fftSize = 128;
+
+      var bufferLength = analyser.frequencyBinCount;
+      var dataArray = new Uint8Array(bufferLength);
+
+      for (i = 0; i < bufferLength; i++) {
         point = new Point();
         points.push(point);
       }
@@ -74,50 +95,53 @@ function init() {
       canvasW = canvas.width;
       canvasH = canvas.height;
 
+      function draw() {
+        var ctx = document.getElementById('main').getContext('2d');
+
+        ctx.globalCompositeOperation = 'destination-over';
+        ctx.clearRect(0, 0, canvasW, canvasH); // clear canvas
+
+        ctx.fillStyle = colors['green'] + globalalpha.toString() + ')';
+        ctx.strokeStyle = colors['green'] + globalalpha.toString() + ')';
+
+        analyser.getByteFrequencyData(dataArray);
+
+        for (i = 0; i < points.length; i++) {
+
+          var point = points[i]
+
+          point.update_velocity();
+          point.update_position(dataArray[i]);
+
+          ctx.beginPath();
+          ctx.arc(canvasW/2 + point.x, canvasH/2 + point.y, dataArray[i]/10, 0, Math.PI * 2, false);
+          ctx.fill();
+          ctx.stroke();
+        }
+
+        ctx.beginPath();
+        ctx.arc(canvasW/2, canvasH/2, 200, 0, Math.PI * 2, false); // Earth orbit
+        ctx.stroke();
+
+        // ctx.font = "30px Arial";
+        // ctx.fillStyle = 'white';
+        // ctx.textAlign = "center";
+        // ctx.fillText("Tom Muir", canvas.width/2, canvas.height/2);
+
+        var grd = ctx.createLinearGradient(0, 0, canvasW*(dataArray[3]/100), canvasH*(dataArray[3]/100));
+        grd.addColorStop(0, colors['pink'] + globalalpha.toString() + ')');
+        grd.addColorStop(1, colors['blue'] + globalalpha.toString() + ')');
+
+        ctx.fillStyle = grd;
+        ctx.fillRect(0, 0, canvasW, canvasH);
+
+        window.requestAnimationFrame(draw);
+      }
+      context.resume();
       window.requestAnimationFrame(draw);
     }
   }
 }
 
-function draw() {
-  var ctx = document.getElementById('main').getContext('2d');
-
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0, 0, canvasW, canvasH); // clear canvas
-
-  ctx.fillStyle = colors['green'] + globalalpha.toString() + ')';
-  ctx.strokeStyle = colors['green'] + globalalpha.toString() + ')';
-
-  for (i = 0; i < points.length; i++) {
-
-    var point = points[i]
-
-    point.update_velocity();
-    point.update_position();
-
-    ctx.beginPath();
-    ctx.arc(canvasW/2 + point.x, canvasH/2 + point.y, 10, 0, Math.PI * 2, false); // Earth orbit
-    ctx.fill();
-    ctx.stroke();
-  }
-
-  ctx.beginPath();
-  ctx.arc(canvasW/2, canvasH/2, 200, 0, Math.PI * 2, false); // Earth orbit
-  ctx.stroke();
-
-  ctx.font = "30px Arial";
-  ctx.fillStyle = 'white';
-  ctx.textAlign = "center";
-  ctx.fillText("Tom Muir", canvas.width/2, canvas.height/2);
-
-  // var grd = ctx.createLinearGradient(0, 0, canvasW, canvasH);
-  // grd.addColorStop(0, colors['pink'] + globalalpha.toString() + ')');
-  // grd.addColorStop(1, colors['blue'] + globalalpha.toString() + ')');
-  //
-  // ctx.fillStyle = grd;
-  // ctx.fillRect(0, 0, canvasW, canvasH);
-
-  window.requestAnimationFrame(draw);
-}
 
 init();
